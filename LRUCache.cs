26d9776362aeb3620b4301f584546ea74151d6db
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-18 22:06:23 +0300

    Добавьте файлы проекта.

---
+++ LRUCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+	internal class LRUCache
+	{
+		public LRUCache(int capacity)
+		{
+
+		}
+
+		public int Get(int key)
+		{
+
+		}
+
+		public void Put(int key, int value)
+		{
+
+		}
+	}
+}
