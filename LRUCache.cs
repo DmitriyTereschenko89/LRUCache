e78a990326e89c4ad430f234bd730af37940a4f0
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-18 23:20:21 +0300

    update read.me

--- LRUCache.cs
+++ LRUCache.cs
@@ -8,19 +8,136 @@ namespace LRUCache
 {
 	internal class LRUCache
 	{
+		private class DoubleLinkedNode
+		{
+			public int key;
+			public int val;
+			public DoubleLinkedNode prev;
+			public DoubleLinkedNode next;
+
+			public DoubleLinkedNode(int key, int val, DoubleLinkedNode prev = null, DoubleLinkedNode next = null)
+			{
+				this.key = key;
+				this.val = val;
+				this.prev = prev;
+				this.next = next;
+			}
+
+			public void RemoveBinding()
+			{
+				if (this.prev != null)
+				{
+					this.prev.next = this.next;
+				}
+				if (this.next != null)
+				{
+					this.next.prev = this.prev;
+				}
+				this.next = null;
+				this.prev = null;
+			}
+		}
+
+		private class DoubleLinkedList
+		{
+			public DoubleLinkedNode head;
+			public DoubleLinkedNode tail;
+
+			public void SetHead(DoubleLinkedNode node)
+			{
+				if (head == node)
+				{
+					return;
+				}
+				if (head is null)
+				{
+					head = node;
+					tail = node;
+				}
+				else if (head == tail)
+				{
+					tail.prev = node;
+					head = node;
+					head.next = tail;
+				}
+				else
+				{
+					if (tail == node)
+					{
+						RemoveTail();
+					}
+					node.RemoveBinding();
+					head.prev = node;
+					node.next = head;
+					head = node;
+				}
+			}
+
+			public void RemoveTail()
+			{
+				if (tail is null)
+				{
+					return;
+				}
+				if (head == tail)
+				{
+					head = null;
+					tail = null;
+					return;
+				}
+				tail = tail.prev;
+				tail.next = null;
+			}
+		}
+
+		private readonly int capacity;
+		private readonly Dictionary<int, DoubleLinkedNode> lruCache;
+		private readonly DoubleLinkedList linkedList;
+
+		private void Update(DoubleLinkedNode node)
+		{
+			linkedList.SetHead(node);
+		}
+
+		private void Remove()
+		{
+			int key = linkedList.tail.key;
+			linkedList.RemoveTail();
+			lruCache.Remove(key);
+		}
+
 		public LRUCache(int capacity)
 		{
-
+			this.capacity = capacity;
+			lruCache = new Dictionary<int, DoubleLinkedNode>();
+			linkedList = new DoubleLinkedList();
 		}
 
 		public int Get(int key)
 		{
-
+			if (!lruCache.ContainsKey(key))
+			{
+				return -1;
+			}
+			Update(lruCache[key]);
+			return lruCache[key].val;
 		}
 
 		public void Put(int key, int value)
 		{
-
+			if (!lruCache.ContainsKey(key))
+			{
+				if (lruCache.Count == capacity)
+				{
+					Remove();
+				}
+				lruCache.Add(key, new DoubleLinkedNode(key,value));
+			}
+			else
+			{
+				lruCache[key].val = value;
+			}
+			Update(lruCache[key]);
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+LRUCache.LRUCache lRUCache1 = new(2);
+lRUCache1.Put(1, 1); // cache is {1=1}
+lRUCache1.Put(2, 2); // cache is {1=1, 2=2}
+Console.WriteLine(lRUCache1.Get(1));    // return 1
+lRUCache1.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+Console.WriteLine(lRUCache1.Get(2));     // returns -1 (not found)
+lRUCache1.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+Console.WriteLine(lRUCache1.Get(1));    // return -1 (not found)
+Console.WriteLine(lRUCache1.Get(3));    // return 3
+Console.WriteLine(lRUCache1.Get(4));    // return 4
+Console.WriteLine("=================");
+LRUCache.LRUCache lRUCache2 = new(2);
+lRUCache2.Put(1, 0); // cache is {1=0}
+lRUCache2.Put(2, 2); // cache is {1=0, 2=2}
+Console.WriteLine(lRUCache2.Get(1));    // return 0
+lRUCache2.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=0, 3=3}
+Console.WriteLine(lRUCache2.Get(2));     // returns -1 (not found)
+lRUCache2.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+Console.WriteLine(lRUCache2.Get(1));    // return -1 (not found)
+Console.WriteLine(lRUCache2.Get(3));    // return 3
+Console.WriteLine(lRUCache2.Get(4));    // return 4
+	
